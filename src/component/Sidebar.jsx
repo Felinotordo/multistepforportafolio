7c10ba359b9b4fb32be7b4d43ce9c62606f0b4f7
Felinotordo <felinotordo99@gmail.com>
2024-10-20 20:16:11 -0300

    adding desktop desing

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -78,64 +78,80 @@ const Sidebar = ({ children }) => {
       content = <div>invalid step</div>;
   }
 
+  const steps = [
+    { num: 1, title: "YOUR INFO" },
+    { num: 2, title: "SELECT PLAN" },
+    { num: 3, title: "ADD-ONS" },
+    { num: 4, title: "SUMMARY" },
+  ];
+
   return (
-    <div className="w-screen h-screen flex flex-col relative">
-      <div
-        style={{
-          backgroundImage: `url("/bg-sidebar-mobile.svg")`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-        className="flex space-x-2 justify-center w-full h-[180px] pt-[30px]"
-      >
-        {[1, 2, 3, 4].map((num) => (
-          <span
-            key={num}
-            className={`w-8 h-8 flex items-center justify-center rounded-full ${
-              step === num || (step === 5 && num === 4)
-                ? "bg-[#bbdff7]"
-                : "bg-[#483dff] border-[1px] border-white text-white"
-            }`}
-          >
-            {num}
-          </span>
-        ))}
-      </div>
-
-      <div className="flex flex-col items-center justify-center absolute top-[100px] left-0 right-0 mx-auto">
-        <div className="bg-white w-[350px] min-h-[380px] h-auto rounded-[15px] shadow-xl p-6">
-          {content}
+    <div className="w-screen h-screen flex justify-center items-center bg-[#eef5ff]">
+      <div className="w-screen h-screen flex flex-col relative sm:flex-row sm:w-[800px] sm:h-[600px] sm:bg-white sm:rounded-[20px]">
+        <div
+          className="flex space-x-2 justify-center w-full h-[180px] pt-[30px] bg-mobile-sidebar bg-no-repeat bg-center bg-cover sm:bg-desktop-sidebar sm:w-[350px] sm:bg-cover sm:h-[500px] sm:rounded-[10px] sm:m-[16px] sm:flex-col sm:justify-start
+          sm:space-x-0 sm:space-y-[25px] sm:items-start"
+        >
+          {steps.map(({ num, title }) => (
+            <div
+              key={num}
+              className="flex sm:items-center sm:justify-between sm:text-white sm:font-ubuntu"
+            >
+              <span
+                className={`size-8 sm:size-7 flex items-center justify-center rounded-full sm:ml-[32px] sm:mr-[16px] text-white ${
+                  step === num || (step === 5 && num === 4)
+                    ? "bg-[#bbdff7] text-black"
+                    : "bg-[#483dff] border-[1px] border-white text-white"
+                }`}
+              >
+                {num}
+              </span>
+              <div className="hidden sm:block sm:flex-col sm:items-center">
+                <p className="hidden font-ubuntu sm:block sm:text-[#a7a6fe] sm:text-[10px]">
+                  STEP {num}
+                </p>
+                <p className="hidden font-ubuntu sm:block sm:text-[12px]">
+                  {title}
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
-      </div>
-
-      <div className="bg-[#edf4fe] flex-grow"></div>
-
-      <div
-        className={`flex ${
-          step === 1 || step === 5 ? "justify-end" : "justify-between"
-        } w-full bg-white p-4`}
-      >
-        <button
-          onClick={() => {
-            backStep();
-          }}
-          className={`text-[#9e9ea5] font-ubuntu font-medium ${step === 1 ? "hidden" : ""} ${
-            step === 5 ? "hidden" : ""
-          }`}
-        >
-          Go back
-        </button>
-        <button
-          onClick={() => {
-            nextStep();
-          }}
-          className={`w-[100px] h-[40px] ${
-            step < 4 ? "bg-[#04285a]" : "bg-[#483eff]"
-          } text-white rounded-lg`}
-        >
-          {step === 4 ? "Confirm" : step === 5 ? "Restart" : "Next Step"}
-        </button>
+        <div className="flex flex-col h-screen sm:h-0 sm:items-center sm:w-full sm:m-[16px]">
+          <div className="flex flex-col items-center justify-center absolute top-[100px] left-0 right-0 mx-auto sm:static">
+            <div className="bg-white w-[350px] min-h-[380px] h-auto rounded-[15px] sm:rounded-none sm:shadow-none shadow-xl p-6">
+              {content}
+            </div>
+          </div>
+          <div className="bg-[#edf4fe] flex-grow"></div>{" "}
+          {/* Esto permite que el espacio se llene */}
+          <div
+            className={`flex ${
+              step === 1 || step === 5 ? "justify-end" : "justify-between"
+            } w-full bg-white p-4`}
+          >
+            <button
+              onClick={() => {
+                backStep();
+              }}
+              className={`text-[#9e9ea5] font-ubuntu font-medium w-[100px] h-[50px] ${
+                step === 1 ? "hidden" : ""
+              } ${step === 5 ? "hidden" : ""}`}
+            >
+              Go back
+            </button>
+            <button
+              onClick={() => {
+                nextStep();
+              }}
+              className={`w-[100px] h-[40px] ${
+                step < 4 ? "bg-[#04285a]" : "bg-[#483eff]"
+              } text-white rounded-lg`}
+            >
+              {step === 4 ? "Confirm" : step === 5 ? "Restart" : "Next Step"}
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      backgroundImage:{
+        "mobile-sidebar": "url('/public/bg-sidebar-mobile.svg')",
+        "desktop-sidebar": "url('/public/bg-sidebar-desktop.svg')",
+      },
       fontFamily: {
         ubuntu: ["Ubuntu", "sans-serif"],
       },
